864fe2aa203a3207aad0e20e391d9a9ae93b8ecd
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2018-04-07 18:48:40 +0200

    add exemple

--- test_async.js
+++ test_async.js
@@ -1,21 +1,39 @@
 'use strict';
 
-function test(){
+
+//generate error for try catch fail
+function makeError(){
     let errorMsg = new Error("ERROR HIT !")
     throw errorMsg;
 }
 
 
-async function run(){
+//generate simple message
+function makeMsg(){
+    let msg = "one message !"
+    console.log(msg)
+    return msg;
+}
+
+
+//function just generate message
+
+async function runTest(){
     try {
-        await test();
+        await makeError(); // will enter into the catch
+
+        //this line will never call because makeError enter into the catch 
+        await makeMsg();
+
     } catch (err) {
-        console.log(err.message)
+        console.log(err) //display information (line etc)
+        console.log(`CATCH TRIGGERRRRR ${err.message} `)
+    
     }
    
     return "run did his job !"; 
 }
 
 
-run()
+runTest()
 
